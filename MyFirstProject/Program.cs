d4470e0802991a8f2cd33e883e6ba6d32de6dd67
Oreinz M. Kasilag <oreinzk@yahoo.com>
2022-12-04 13:29:42 +0800

    Tryiing to parse

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -5,54 +5,21 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        bool isRegistred = true;
-        String username = "", password = "";
+        String a = Console.ReadLine();
+        int number, numTemp;
+        bool tryStrToInt = int.TryParse(a, out number);
 
-        while(isRegistred) {
-
-
-            Console.WriteLine("Please enter your username");
-            username = Console.ReadLine();
-
-            Console.WriteLine("Please enter your password");
-            password = Console.ReadLine();
-
-            if(username != "" && password != "")
-            {
-                Console.WriteLine("You are login");
-                isRegistred= false;
-            }
-            else
-            {
-                Console.WriteLine("Invalid username and password");
-            }
-        }
-
-        Console.WriteLine($"\n\nYour username is {username} and your password is {password}");
-
-        Console.ReadKey();
-
-    }
-
-    public static String Weather(int temp) { 
-        
-        if(temp < 10 && temp != 0){
-
-            return "Take the coat";
-        
-        }else if (temp == 10){
-
-            return "It's 10 degrees C";
-
-        }
-        else if(temp > 10){
-
-            return "It's 10 degrees C";
+        if (tryStrToInt)
+        {
+            numTemp= number;
+            Console.WriteLine(numTemp);
         }
         else
         {
-            return "Not a number";
+            numTemp= 0;
         }
-    
+        
+
     }
+
 }
