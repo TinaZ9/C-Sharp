ab46f346cf4310e0aefb1615c2f8584a437e2c5b
Oreinz M. Kasilag <oreinzk@yahoo.com>
2022-12-04 20:14:11 +0800

    C# Class

---
+++ MyFirstProject/Human.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProject
+{
+    //this class is a blueprint for a datatype
+    internal class Human
+    {
+
+
+        public string Fullname(string firstName, string lastName)
+        {
+            return $"{firstName}\t{lastName}";
+        }
+
+    }
+}
