3ce7640a204b88b79afb44fc6fe4502cfa8ba541
Oreinz M. Kasilag <oreinzk@yahoo.com>
2022-12-25 10:46:39 +0800

    just add some stuff

---
+++ ManpowerApp/ManpowerApp/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using ManpowerApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ManpowerApp
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
---
+++ ManpowerApp/ManpowerlistingApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,52 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ManpowerlistingApp.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _username;
+        private string _password;
+
+
+        public string Username { 
+            get 
+            { 
+                return _username;
+            }
+            set 
+            {
+                _username = value;    
+            
+            } 
+        }
+
+        public string Password 
+        {
+            get 
+            {
+                return _password;
+            }
+            set 
+            { 
+                
+                _password = value; 
+            
+            
+            }
+        
+        }
+
+        public void Click()
+        {
+
+            MessageBox.Show(Username + " " + Password);
+        }
+
+    }
+}
